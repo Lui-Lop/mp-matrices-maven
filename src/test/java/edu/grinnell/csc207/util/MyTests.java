cc6b98a05954515eb4e082109387bc8fcc6404d9
Luis Lopez <ruk@Luiss-MacBook-Air.local>
2024-10-19 17:13:51 -0500

    Start of Tests

--- src/test/java/edu/grinnell/csc207/util/MyTests.java
+++ src/test/java/edu/grinnell/csc207/util/MyTests.java
@@ -14,17 +14,107 @@ import org.junit.jupiter.api.Test;
 
 class MyTests {
     
-    get
-    set
-    height
-    width
-    insertRow
-    insertRow []
-    insertCol
-    insertCol []
-    deleteRow
-    deleteCol
-    fillRegion
-    fillLine
-    equals
+    /**
+     * 
+     */
+    @Test
+    public void testGet() {
+        
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testSet() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testHeight() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testWidth() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testInsertRow() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testInsertRowArr() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testInsertCol() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testInsertColArr() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testDeleteRow() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testDeleteCol() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testFillReg() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testFillLine() {
+
+    }
+
+        /**
+     * 
+     */
+    @Test
+    public void testEquals() {
+
+    }
 }
