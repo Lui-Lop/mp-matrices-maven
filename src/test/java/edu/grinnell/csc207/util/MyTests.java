cfcbcb89d582e312ad4008f666736329fd80a505
Luis Lopez <ruk@Luiss-MacBook-Air.local>
2024-10-19 15:03:35 -0500

    Added File for Tests

---
+++ src/test/java/edu/grinnell/csc207/util/MyTests.java
@@ -0,0 +1,30 @@
+package edu.grinnell.csc207.util;
+
+
+import static edu.grinnell.csc207.util.MatrixAssertions.assertMatrixEquals;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+
+class MyTests {
+    
+    get
+    set
+    height
+    width
+    insertRow
+    insertRow []
+    insertCol
+    insertCol []
+    deleteRow
+    deleteCol
+    fillRegion
+    fillLine
+    equals
+}
